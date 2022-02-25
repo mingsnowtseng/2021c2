b035f7d06b5a9f41852b0f45e1bf852d5f366a89
mingsnowtseng <aa503164@gmail.com>
2022-02-25 12:22:32 +0800

    add week01

---
+++ week01/week01-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    char n;
+    scanf("%c",&n);
+    printf("Åª¨ì¦r¤¸:%c\n",n);
+
+    char line[1000];
+    scanf("%s",line);
+    printf("Åª¨ì¦r¦ê:%s\n",line);
+    return 0;
+}
